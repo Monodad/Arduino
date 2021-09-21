750f87b7c1576e3d4ed3c23fe1e9d24b8bac62d0
Monodad <a5710168@gmail.com>
2021-09-21 20:14:44 +0800

    This is not god

--- arduino/ADXL345.ino
+++ arduino/ADXL345.ino
@@ -1,26 +1,42 @@
-/*
-  AnalogReadSerial
-
-  Reads an analog input on pin 0, prints the result to the Serial Monitor.
-  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/AnalogReadSerial
-*/
-
-// the setup routine runs once when you press reset:
+#include <Wire.h>
+int device_contoll = 0xEB;
+int device_address = 0x53; 
+byte data_address[] = {0x32,0x33,0x34,0x35,0x36,0x37};
+int reg_read(byte address , byte register_address){
+  Wire.beginTransmission(address);
+  Wire.write(register_address);
+  Wire.endTransmission(false);
+  Wire.requestFrom(address,1);
+   while(Wire.available())    // slave may send less than requested
+  {   
+    Serial.print(Wire.read(),BIN);
+    Serial.print("\n"); 
+    // print the character
+  }
+  Wire.endTransmission();
+}
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
+  Wire.begin();
+  Wire.setClock(400000);
+  Wire.beginTransmission(0x53);
+  Wire.write(0x00);
+  Wire.endTransmission(false);
+  Wire.requestFrom(0x53,1);
+   while(Wire.available())    // slave may send less than requested
+  {   
+    Serial.print(Wire.read(),BIN);         // print the character
+  }
+  Serial.print("\n"); 
+  delay(1000);
+  Wire.endTransmission();
 }
-
+int count = 0;
 // the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
-  // print out the value you read:
-  Serial.println(sensorValue);
-  delay(1);        // delay in between reads for stability
+  for(int i = 0 ; i<8;i++){
+    reg_read(device_address , data_address[i]);
+  }
+  Serial.print("next\n");
 }
