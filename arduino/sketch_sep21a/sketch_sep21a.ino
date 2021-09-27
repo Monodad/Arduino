ebb009563019665371c876ef6cc36275532893f0
Monodad <a5710168@gmail.com>
2021-09-27 21:02:32 +0800

    Co-authored-by: WeedForJustice <WeedForJustice@users.noreply.github.com>

--- arduino/sketch_sep21a/sketch_sep21a.ino
+++ arduino/sketch_sep21a/sketch_sep21a.ino
@@ -1,26 +1,9 @@
 #include <Wire.h>
-int device_contoll = 0xEB;
-int device_address = 0x53; 
-byte data_address[] = {0x32,0x33,0x34,0x35,0x36,0x37};
-String txt[] = {"\nX : ","\nY : ","\nZ : "};
-int set = '01101011';
-int reg_read(byte address , byte register_address){
-  Wire.beginTransmission(address);
-  Wire.write(register_address);
-  Wire.endTransmission(false);
-  Wire.requestFrom(address,1);
-   while(Wire.available())    // slave may send less than requested
-  {   
-    Serial.print(Wire.read(),DEC);
+const int device_address = 0x53;
 
-    // print the character
-  }
-  Serial.print(" ");
-  Wire.endTransmission();
-}
 void setup() {
   // initialize serial communication at 9600 bits per second:
-  
+
   Serial.begin(9600);
   Serial.print("Device ID : ");
   Wire.begin();
@@ -28,28 +11,28 @@ void setup() {
   Wire.beginTransmission(0x53);
   Wire.write(0x00);
   Wire.endTransmission(false);
-  Wire.requestFrom(0x53,1);
-   while(Wire.available())    // slave may send less than requested
-  {   
-    Serial.print(Wire.read(),BIN);         // print the character
+  Wire.requestFrom(0x53, 1);
+  while (Wire.available())   // slave may send less than requested
+  {
+    Serial.print(Wire.read(), BIN);        // print the character
   }
-  Serial.print("\n"); 
+  Serial.print("\n");
   delay(1000);
   Wire.endTransmission();
   Serial.print("Set x31 value\n");
   Wire.beginTransmission(0x53);
   Wire.write(0x31);
-  Wire.write(0x69);
+  Wire.write(0b01101000);
   Wire.endTransmission();
 
-Serial.print("Get x31 value : ");
-   Wire.beginTransmission(0x53);
+  Serial.print("Get x31 value : ");
+  Wire.beginTransmission(0x53);
   Wire.write(0x31);
   Wire.endTransmission(false);
-  Wire.requestFrom(0x53,1);
-   while(Wire.available())    // slave may send less than requested
-  {   
-    Serial.print(Wire.read(),BIN);         // print the character
+  Wire.requestFrom(0x53, 1);
+  while (Wire.available())   // slave may send less than requested
+  {
+    Serial.print(Wire.read(), BIN);        // print the character
   }
   Serial.print("\n");
   delay(1000);
@@ -60,15 +43,41 @@ Serial.print("Get x31 value : ");
   Wire.write(0x2D);
   Wire.write(0x28);
   Wire.endTransmission();
-
 }
-int count = 0;
-// the loop routine runs over and over again forever:
-void loop() {
-  for(int i = 0 ; i<6;i++){
-    reg_read(device_address , data_address[i]);
-      Serial.print(txt[i/2]); 
-    }
 
-    delay(100);
+void loop() {
+   int *xyz = reg_read(device_address);
+  
+  for (int i = 0; i < 3; i++) {
+    Serial.println(xyz[i]);
+  }
+  
+  delay(1000);
+}
+
+
+
+ int *reg_read(byte address) {
+  Wire.beginTransmission(address);
+  Wire.write(0x32);
+  Wire.endTransmission(false);
+  Wire.requestFrom(address, 6);
+
+  byte data[6];
+  byte i = 0;
+  while (Wire.available() && i++ < 6){
+    data[i] = Wire.read();
+    Serial.print("TEST: ");
+    Serial.println(data[i]);
+  }
+  Wire.endTransmission();
+
+  static  int xyz[3];
+  for (int i = 0; i < 3; i++) {
+    xyz[i] = data[i*2 + 1];
+    xyz[i] <<= 8;
+    xyz[i] |= data[i*2];
+  }
+
+  return xyz;
 }
